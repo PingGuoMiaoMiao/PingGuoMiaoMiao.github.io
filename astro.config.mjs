da03bf32b41d09f89f888244d8bc0f3176d0b3f7
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2024-05-01 12:42:21 +0800

    modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/Greeting.jsx 	modified:   src/components/Header.astro new file:   src/components/ThemeIcon.astro 	modified:   src/pages/about.astro 	modified:   src/pages/index.astro 	modified:   src/styles/global.css 	modified:   tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com"
+  site: "https://example.com",
+  integrations: [preact()]
 });
